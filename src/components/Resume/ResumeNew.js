24fb3c18f9ac02e443fdd176abb08d0911870e03
Rahul G <rahulgworking@gmail.com>
2022-02-12 23:28:29 +0530

    uploaded resume8

--- src/components/Resume/ResumeNew.js
+++ src/components/Resume/ResumeNew.js
@@ -8,8 +8,7 @@ import { Document, Page, pdfjs } from "react-pdf";
 import "react-pdf/dist/esm/Page/AnnotationLayer.css";
 pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.min.js`;
 
-const resumeLink =
-  "../../Assets/Rahul_G_resume_2022.pdf";
+const resumeLink ="../../Assets/Rahul_G_resume_2022.pdf";
 
 function ResumeNew() {
   const [width, setWidth] = useState(1200);
@@ -30,7 +29,7 @@ function ResumeNew() {
         </Row>
 
         <Row className="resume">
-          <Document file={resumeLink}>
+          <Document file={pdf}>
             <Page pageNumber={1} scale={width > 786 ? 1.7 : 0.6} />
           </Document>
         </Row>
